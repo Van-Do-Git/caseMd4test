29953326a017f3c603bc49d13b8ae01ac83cd4c0
unknown <mittervan@gmail.com>
2021-10-07 14:31:54 +0700

    first commit

---
+++ src/main/java/com/codegym/casemd4/service/post/IServicePost.java
@@ -0,0 +1,8 @@
+package com.codegym.casemd4.service.post;
+
+
+import com.codegym.casemd4.model.Post;
+import com.codegym.casemd4.service.IGeneralService;
+
+public interface IServicePost extends IGeneralService<Post> {
+}
